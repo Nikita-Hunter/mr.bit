c1e82a83f18acebd0ce3bbf90330c8b154dff7ef
nayskom <50783295+nayskom@users.noreply.github.com>
2021-11-18 12:10:07 +0300

    make hero section

--- src/js/global.js
+++ src/js/global.js
@@ -2,4 +2,4 @@ var cityMenu = document.getElementById('cityMenu')
 var cityWrap = document.getElementById('cityWrap')
 cityMenu.onclick = function(){
     cityWrap.classList.toggle('visible-city')
-}
+};
